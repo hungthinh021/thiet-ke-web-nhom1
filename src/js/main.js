f41e6ebc7fc267b525ace3ed912f1eb437ee5f7e
tranduc2612 <mintduc2612@gmail.com>
2021-10-30 18:23:52 +0700

    slider web

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,72 @@
 
+
+
+const app = {
+    render: function(){
+
+    },
+
+    handleSlider: function(){
+        const slides = document.querySelectorAll('.slide')
+        const slideBtn = document.querySelectorAll('.slider__btn')
+        let currentSlide = 1;
+
+
+        // manual
+        const manualNav = function(manual){
+            slides.forEach(slide=>{
+                slide.classList.remove('active')
+
+                slideBtn.forEach(btn=>{
+                    btn.classList.remove('active')
+                })
+            })
+
+            slides[manual].classList.add('active')
+            slideBtn[manual].classList.add('active')
+        }
+
+        slideBtn.forEach((btn,index)=>{
+            btn.addEventListener('click', ()=>{
+                manualNav(index);
+                currentSlide = index;
+            })
+        })
+
+        // auto
+
+        const repeat = function(activeClass){
+            let activeSlides = document.getElementsByClassName('active');
+            let i = 1;
+
+            let repeater = ()=>
+            {
+                setTimeout(function(){
+                    [...activeSlides].forEach(activeSlide=>{
+                        activeSlide.classList.remove('active');
+                    })
+
+                    slides[i].classList.add('active')
+                    slideBtn[i].classList.add('active')
+                    i++
+                    
+                    if(i == slides.length){ 
+                        i = 0;
+                    }
+                    if(i >= slides.length){
+                        return;
+                    }
+                    repeater()
+                },10000)
+            }
+            repeater()
+        }
+        repeat()
+    },
+
+    start: function(){
+        this.handleSlider();
+    }
+}
+
+app.start();
